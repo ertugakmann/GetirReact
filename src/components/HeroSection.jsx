36517b1e8657f0c2a0524c07f3ca6cf9e14e61f8
ertug <ertug3470@gmail.com>
2024-03-18 23:39:44 +0300

    The sign in box added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
+import ReactFlagsSelect from "react-flags-select";
+import { FaFacebook } from "react-icons/fa6";
 
 function HeroSection() {
+  const [selected, setSelected] = useState("TR");
+
+  const phones = {
+    US: "+1",
+    DE: "+50",
+    TR: "+90",
+    IT: "+7",
+    IN: "+15",
+  };
+
   var settings = {
     dots: false,
     infinite: true,
@@ -10,6 +22,9 @@ function HeroSection() {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
+    autoplay: true,
+    speed: 2500,
+    autoplaySpeed: 3500,
   };
 
   return (
@@ -46,10 +61,36 @@ function HeroSection() {
             Dakikalar içinde kapında
           </h3>
         </div>
+
         <div className="w-[400px] rounded-lg bg-gray-50 p-6">
-          <h4 className="text-primary-brand-color font-semibold text-center ">
+          <h4 className="text-primary-brand-color font-semibold text-center mb-5 ">
             Giriş yap veya kayıt ol
           </h4>
+          <div className="grid gap-y-3">
+            <div className="flex gap-x-2">
+              <ReactFlagsSelect
+                countries={Object.keys(phones)}
+                customLabels={phones}
+                selected={selected}
+                onSelect={(code) => setSelected(code)}
+                className="flag-select"
+              />
+              <div className="flex-1 relative">
+                <input
+                  placeholder="Telefon Numarası"
+                  className="h-11 px-4 border-2 border-gray-200  rounded w-full transition-colors hover:border-primary-brand-color outline-none"
+                />
+              </div>
+            </div>
+            <button className="bg-brand-yellow h-12 flex items-center justify-center transition-colors  hover:bg-primary-brand-color hover:text-brand-yellow rounded-md w-full text-brand-color font-semibold">
+              Telefon numarası ile devam et
+            </button>
+            <div className="h-[1px] bg-gray-300 my-3"></div>
+            <button className="bg-blue-700 bg-opacity-10 text-blue-700 text-opacity-80 h-12 flex items-center justify-center transition-colors  hover:bg-primary-brand-color hover:text-brand-yellow rounded-md w-full text-brand-color font-semibold">
+              <FaFacebook className="mr-2" size={18} />
+              Facebook ile devam et
+            </button>
+          </div>
         </div>
       </div>
     </div>
